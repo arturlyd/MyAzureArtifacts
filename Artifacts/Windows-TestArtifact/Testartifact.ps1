bfee49b4a81c85e5bd5122541d6e65bef84348b7
arturlyd <arturlyd@users.noreply.github.com>
2018-06-21 12:30:07 -0500

    test

---
+++ Artifacts/Windows-TestArtifact/Testartifact.ps1
@@ -0,0 +1,18 @@
+﻿<##################################################################################################
+
+    Description
+    ===========
+
+	- This script does the following - 
+		- creates a txt file in c:\temp
+
+
+
+    Usage examples
+    ==============
+    
+    Powershell -executionpolicy bypass -file ChocolateyPackageInstaller.ps1
+
+
+##################################################################################################>
+New-Item -ItemType directory -Path C:\Paco\Pacofolder
