3eabca8dd3fd773194688f4a442ff96a26950d5b
arturlyd <arturlyd@users.noreply.github.com>
2018-06-26 11:48:06 -0500

    200 artifact

--- Artifacts/Windows-ERP102200-0/ERPDL1022000.ps1
+++ Artifacts/Windows-ERP102200-0/ERPDL1022000.ps1
@@ -4,7 +4,7 @@
     ===========
 
 	- This script does the following - 
-		- Downloads SQL Server 2016 version that we install in QA tools. 
+		- Downloads SQL Server 2017 version that we install in QA tools. 
         it has its own license so we are not charged for that by Azure
 
 
@@ -18,10 +18,12 @@
 $StorageAccountName = "aqatoolslab2420"
 $blobSas = "?sv=2017-11-09&ss=bfqt&srt=sco&sp=rwdlacup&se=2027-06-06T05:11:13Z&st=2018-06-05T21:11:13Z&spr=https,http&sig=vdilQIbevC02X6gu8d%2FQt25%2BUClG7FCRrchlogcFI2Q%3D"
 $storageContext = New-AzureStorageContext $StorageAccountName -SasToken $blobSas
-$containerName = "sqlserver"
+$containerName = "isos"
 $targetDir = 'c:\EpicorInstallers\'
-$blobName = "SQLServer2016STD.7z"
+$blobName = "RL10.2.200.0.iso"
 if(!(Test-Path -Path $targetDir )){
     New-Item -ItemType directory -Path $targetDir
 }
+Get-AzureStorageBlobContent -Container $ContainerName -Blob $blobName -Destination ($targetDir + $blobName) -Context $StorageContext -Force
+$blobName = "Demo32200Build6.bak"
 Get-AzureStorageBlobContent -Container $ContainerName -Blob $blobName -Destination ($targetDir + $blobName) -Context $StorageContext -Force
--- Artifacts/Windows-ERPDLSQL2017/ERPDLSQL2017.ps1
+++ Artifacts/Windows-ERPDLSQL2017/ERPDLSQL2017.ps1
@@ -19,7 +19,7 @@ $StorageAccountName = "aqatoolslab2420"
 $blobSas = "?sv=2017-11-09&ss=bfqt&srt=sco&sp=rwdlacup&se=2027-06-06T05:11:13Z&st=2018-06-05T21:11:13Z&spr=https,http&sig=vdilQIbevC02X6gu8d%2FQt25%2BUClG7FCRrchlogcFI2Q%3D"
 $storageContext = New-AzureStorageContext $StorageAccountName -SasToken $blobSas
 $containerName = "sqlserver"
-$targetDir = 'c:\temp\'
+$targetDir = 'c:\EpicorInstallers\'
 $blobName = "SQLServer2017STD.7z"
 if(!(Test-Path -Path $targetDir )){
     New-Item -ItemType directory -Path $targetDir
