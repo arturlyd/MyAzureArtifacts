80ce52941e11f2eab0fc9b984b3bd641a50650bd
arturlyd <arturlyd@users.noreply.github.com>
2018-06-26 15:45:58 -0500

    100 artifact

--- Artifacts/Windows-ERP102100-0/ERPDL1021000.ps1
+++ Artifacts/Windows-ERP102100-0/ERPDL1021000.ps1
@@ -4,7 +4,7 @@
     ===========
 
 	- This script does the following - 
-		- Downloads ERP 10.2.200.0 and its demo database
+		- Downloads ERP 10.2.100.0 and its demo database
 
 
 
@@ -19,10 +19,10 @@ $blobSas = "?sv=2017-11-09&ss=bfqt&srt=sco&sp=rwdlacup&se=2027-06-06T05:11:13Z&s
 $storageContext = New-AzureStorageContext $StorageAccountName -SasToken $blobSas
 $containerName = "isos"
 $targetDir = 'c:\EpicorInstallers\'
-$blobName = "RL10.2.200.0.iso"
+$blobName = "RL10.2.100.0.iso"
 if(!(Test-Path -Path $targetDir )){
     New-Item -ItemType directory -Path $targetDir
 }
 Get-AzureStorageBlobContent -Container $ContainerName -Blob $blobName -Destination ($targetDir + $blobName) -Context $StorageContext -Force
-$blobName = "Demo32200Build6.bak"
+$blobName = "Demo321000B5.bak"
 Get-AzureStorageBlobContent -Container $ContainerName -Blob $blobName -Destination ($targetDir + $blobName) -Context $StorageContext -Force
