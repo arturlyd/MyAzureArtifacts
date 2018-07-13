dfaca78195224edf4adbb6f1a8e194d1df4bfe84
arturlyd <arturlyd@users.noreply.github.com>
2018-07-13 11:06:50 -0500

    test

--- Artifacts/Windows-ERPDLSQL2017/ERPDLSQL2017.ps1
+++ Artifacts/Windows-ERPDLSQL2017/ERPDLSQL2017.ps1
@@ -14,7 +14,7 @@
     
 
 
-##################################################################################################>
+##################################################################################################> 
 $StorageAccountName = "aqatoolslab2420"
 $blobSas = "?sv=2017-11-09&ss=bfqt&srt=sco&sp=rwdlacup&se=2027-06-06T05:11:13Z&st=2018-06-05T21:11:13Z&spr=https,http&sig=vdilQIbevC02X6gu8d%2FQt25%2BUClG7FCRrchlogcFI2Q%3D"
 $storageContext = New-AzureStorageContext $StorageAccountName -SasToken $blobSas
