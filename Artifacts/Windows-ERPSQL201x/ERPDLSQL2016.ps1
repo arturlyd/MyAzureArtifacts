da34cf684b871d7d3434f630deedf43213beb1e2
arturlyd <arturlyd@users.noreply.github.com>
2018-07-23 15:10:28 -0500

    test

--- Artifacts/Windows-ERPSQL201x/ERPDLSQL2016.ps1
+++ Artifacts/Windows-ERPSQL201x/ERPDLSQL2016.ps1
@@ -7,14 +7,14 @@
 		- Downloads SQL Server 2016 version that we install in QA tools. 
         it has its own license so we are not charged for that by Azure
 
-
-
-    Usage examples
-    ==============
-    
-
-
 ##################################################################################################>
+[CmdletBinding()]
+param(
+    [string] $ssms = $true
+)
+
+
+
 $StorageAccountName = "aqatoolslab2420"
 $blobSas = "?sv=2017-11-09&ss=bfqt&srt=sco&sp=rwdlacup&se=2027-06-06T05:11:13Z&st=2018-06-05T21:11:13Z&spr=https,http&sig=vdilQIbevC02X6gu8d%2FQt25%2BUClG7FCRrchlogcFI2Q%3D"
 $storageContext = New-AzureStorageContext $StorageAccountName -SasToken $blobSas
@@ -86,31 +86,33 @@ catch
     LogError
     break
 }
-
-try{
-    # Set file and folder path for SSMS installer .exe
-    $filepath="$targetDir$blobSSMS"
-    #If SSMS not present, download
-    <#if (!(Test-Path $filepath)){
-    write-host "Downloading SQL Server 2016 SSMS..."
-    $URL = "https://download.microsoft.com/download/3/1/D/31D734E0-BFE8-4C33-A9DE-2392808ADEE6/SSMS-Setup-ENU.exe"
-    $clnt = New-Object System.Net.WebClient
-    $clnt.DownloadFile($url,$filepath)
-    Write-Host "SSMS installer download complete" -ForegroundColor Green
-    
-    }
-    else {
-    
-    write-host "Located the SQL SSMS Installer binaries, moving on to install..."
-    }#>
-    
-    # start the SSMS installer
-    write-host "Beginning SSMS 2016 install..." -nonewline
-    $Parms = " /Install /Quiet /Norestart /Logs log.txt"
-    Start-Process -FilePath $filepath -ArgumentList $Parms -Wait
-}
-catch
+if($ssms -eq $true)
 {
-    LogError
-    break
+    try{
+        # Set file and folder path for SSMS installer .exe
+        $filepath="$targetDir$blobSSMS"
+        #If SSMS not present, download
+        <#if (!(Test-Path $filepath)){
+        write-host "Downloading SQL Server 2016 SSMS..."
+        $URL = "https://download.microsoft.com/download/3/1/D/31D734E0-BFE8-4C33-A9DE-2392808ADEE6/SSMS-Setup-ENU.exe"
+        $clnt = New-Object System.Net.WebClient
+        $clnt.DownloadFile($url,$filepath)
+        Write-Host "SSMS installer download complete" -ForegroundColor Green
+    
+        }
+        else {
+    
+        write-host "Located the SQL SSMS Installer binaries, moving on to install..."
+        }#>
+    
+        # start the SSMS installer
+        write-host "Beginning SSMS 2016 install..." -nonewline
+        $Parms = " /Install /Quiet /Norestart /Logs log.txt"
+        Start-Process -FilePath $filepath -ArgumentList $Parms -Wait
+    }
+    catch
+    {
+        LogError
+        break
+    }
 }
---
+++ Artifacts/Windows-ERPSQL201x/ERPSQL201x.ps1
@@ -0,0 +1,34 @@
+[CmdletBinding()]
+param(
+    [Parameter(Mandatory = $true)]
+    [string] $sqlver = "sql2016",
+    [Parameter(Mandatory = $false)]
+    [string] $ssms = $true
+)
+
+
+
+###################### Main Block ######################
+# Validate-Params
+$UserName = $env:USERNAME
+$secPassword = ConvertTo-SecureString -String "Epicor123" -AsPlainText -Force
+$credential = New-Object System.Management.Automation.PSCredential("$env:COMPUTERNAME\$($UserName)", $secPassword)
+    
+if ($sqlver -eq "sql2016")
+{
+    $command = "$PSScriptRoot\ERPDLSQL2016.ps1 $ssms"
+        
+}
+if ($sqlver -eq "sql2017")
+{
+    $command = "$PSScriptRoot\ERPDLSQL2017.ps1 $ssms"
+}
+try
+{
+    Invoke-Command -ComputerName $env:COMPUTERNAME -Credential $credential -FilePath $command -ArgumentList $PackageList
+}
+catch
+{
+    Handle-LastError
+}
+
