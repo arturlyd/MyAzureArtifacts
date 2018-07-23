dec94260ae0092ffe813a2f8f89caff43837df4f
arturlyd <arturlyd@users.noreply.github.com>
2018-07-23 10:22:18 -0500

    test

--- Artifacts/Windows-ERP102200-0/ERPDL1022000.ps1
+++ Artifacts/Windows-ERP102200-0/ERPDL1022000.ps1
@@ -26,7 +26,7 @@ $StorageAccountName = "aqatoolslab2420"
 $blobSas = "sv=2017-11-09&ss=bfqt&srt=sco&sp=rwdlacup&se=2027-06-06T05:11:13Z&st=2018-06-05T21:11:13Z&spr=https,http&sig=vdilQIbevC02X6gu8d%2FQt25%2BUClG7FCRrchlogcFI2Q%3D"
 $storageContext = New-AzureStorageContext $StorageAccountName -SasToken ("?"+$blobSas)
 $containerName = "isos"
-#$licContainerName = "licenses"
+$licContainerName = "licenses"
 $targetDir = "c:\EpicorInstallers\"
 $dbBackup = "Demo32200Build6.bak"
 $targetSqlUser = "sa"
@@ -36,19 +36,19 @@ $computerName = $env:ComputerName
 $Logfile = ($targetDir + "ERPDL1022000.log")
 $erpVersion = "10.2.200"
 $erpPatch = ".0"
-#$epicorGSM = "epicor"
-#$epicorPass = "epicor"
-#$apppoolUserName = "$env:ComputerName\$env:USERNAME"
-#$erpBinding = "HttpsBinaryUsernameChannel"
+$epicorGSM = "epicor"
+$epicorPass = "epicor"
+$apppoolUserName = "$env:ComputerName\$env:USERNAME"
+$erpBinding = "HttpsBinaryUsernameChannel"
 $appServerName = "ERP102200"
 $sqlDataSource = [System.Data.Sql.SqlDataSourceEnumerator]::Instance.GetDataSources()|Where-Object{$_.ServerName -eq $env:COMPUTERNAME}
 $sqlInstance = $sqlDataSource.ServerName + "\" +  $sqlDataSource.InstanceName
 $sqlFilesLoc = "c:\SQLFiles\"
-#$ssrsDBName = "SSRS"
-#$ssrsServerInstallPath = "C:\Program Files\Microsoft SQL Server Reporting Services\SSRS\ReportServer"
-#$ssrsBaseURL = "http://$env:ComputerName/ReportServer"
-#$licenseID = "115506.lic"
-#$erpInstallPatch = "C:\Epicor\Erp10\" #pending to be supported
+$ssrsDBName = "SSRS"
+$ssrsServerInstallPath = "C:\Program Files\Microsoft SQL Server Reporting Services\SSRS\ReportServer"
+$ssrsBaseURL = "http://$env:ComputerName/ReportServer"
+$licenseID = "115506.lic"
+$erpInstallPatch = "C:\Epicor\Erp10\" #pending to be supported
 
 if(!(Test-Path -Path $targetDir )){
     New-Item -ItemType directory -Path $targetDir
