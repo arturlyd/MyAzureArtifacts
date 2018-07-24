cd1e7a3845366553f513cc9b71b91e17edca7829
arturlyd <arturlyd@users.noreply.github.com>
2018-07-24 10:08:44 -0500

    test

--- Artifacts/Windows-ERPSQL201x/ERPSQL201x.ps1
+++ Artifacts/Windows-ERPSQL201x/ERPSQL201x.ps1
@@ -13,17 +13,15 @@ param(
 $UserName = $env:USERNAME
 $secPassword = ConvertTo-SecureString -String "Epicor123" -AsPlainText -Force
 $credential = New-Object System.Management.Automation.PSCredential("$env:COMPUTERNAME\$($UserName)", $secPassword)
-write-host $PSCmdlet.SessionState.Path.CurrentLocation.Path
-write-host "folder del script:  $PSScriptRoot"
 if ($sqlver -eq "sql2016")
 {
-    $command = "$PSScriptRoot\ERPDLSQL2016.ps1 $ssms"
+    .\ERPDLSQL2016.ps1 $ssms
 
 }
 if ($sqlver -eq "sql2017")
 {
-    $command = "$PSScriptRoot\ERPDLSQL2017.ps1 $ssms"
+    .\ERPDLSQL2017.ps1 $ssms
 }
-Invoke-Command -ComputerName $env:COMPUTERNAME -Credential $credential -FilePath $command -ArgumentList $PackageList
+#Invoke-Command -ComputerName $env:COMPUTERNAME -Credential $credential -FilePath $command -ArgumentList $PackageList
 
 
